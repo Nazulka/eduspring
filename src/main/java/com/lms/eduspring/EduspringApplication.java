3955ff179cfa2a080f2bbefc35fd2f89b57e62f7
Nazee <72733276+Nazulka@users.noreply.github.com>
2025-09-14 20:31:42 +0100

    Initial Spring Boot project with Gradle

---
+++ src/main/java/com/lms/eduspring/EduspringApplication.java
@@ -0,0 +1,13 @@
+package com.lms.eduspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lms/eduspring/EduspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lms.eduspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
