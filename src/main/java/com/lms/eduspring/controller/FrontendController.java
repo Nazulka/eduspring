0746ddfcfd0eb460da655819b738aa6ea04f1c9e
Nazee <72733276+Nazulka@users.noreply.github.com>
2025-10-11 15:13:59 +0100

    Add FrontendController

--- src/main/java/com/lms/eduspring/controller/FrontendController.java
+++ src/main/java/com/lms/eduspring/controller/FrontendController.java
@@ -6,6 +6,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class FrontendController {
 
+    @GetMapping("/")
+    public String homePage() {
+        // Spring automatically looks for 'home.html' in the templates folder.
+        System.out.println("Serving the Home Page.");
+        return "home";
+    }
+
     @GetMapping("/login")
     public String loginPage() {
         return "login"; // looks for login.html templates folder
