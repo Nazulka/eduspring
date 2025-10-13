ba7998eda5b7fcc93bbb152ffc8e5c0d461ef18b
Nazee <72733276+Nazulka@users.noreply.github.com>
2025-10-13 21:35:46 +0100

    fix login/register flow

--- src/main/java/com/lms/eduspring/controller/FrontendController.java
+++ src/main/java/com/lms/eduspring/controller/FrontendController.java
@@ -1,25 +1,21 @@
 package com.lms.eduspring.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FrontendController {
 
-    @GetMapping("/")
-    public String homePage() {
-        // Spring automatically looks for 'home.html' in the templates folder.
-        System.out.println("Serving the Home Page.");
-        return "home";
-    }
-
-    @GetMapping("/login")
-    public String loginPage() {
-        return "login"; // looks for login.html templates folder
-    }
-
-    @GetMapping("/register")
-    public String registerPage() {
-        return "register"; // looks for register.html
-    }
+//    @GetMapping("/login")
+//    public String loginPage(Model model) {
+//        // only add form object if using Thymeleaf binding
+//        return "login";
+//    }
+//
+//    @GetMapping("/register")
+//    public String registerPage(Model model) {
+//        // only add form object if using Thymeleaf binding
+//        return "register";
+//    }
 }
