b656025dc28989c943f37449212d6d9f115e62da
Nazee Kadiu <72733276+Nazulka@users.noreply.github.com>
2025-10-19 09:06:46 +0100

    Revert "cleanup, remove unused files"

---
+++ src/main/java/com/lms/eduspring/controller/FrontendController.java
@@ -0,0 +1,21 @@
+package com.lms.eduspring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FrontendController {
+
+//    @GetMapping("/login")
+//    public String loginPage(Model model) {
+//        // only add form object if using Thymeleaf binding
+//        return "login";
+//    }
+//
+//    @GetMapping("/register")
+//    public String registerPage(Model model) {
+//        // only add form object if using Thymeleaf binding
+//        return "register";
+//    }
+}
